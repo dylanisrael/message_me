dcf4cdec449f704a80c64f078661035345f9e6b0
dylanisrael <israelmink98@gmail.com>
2022-05-07 18:46:32 -0400

    adding send messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save {self.username = username.downcase}
   validates :username, uniqueness: true, presence: true, length: {minimum: 3, maximum: 15}
   has_many :messages
   has_secure_password
